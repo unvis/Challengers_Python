a394ae160a7f6e3cce4bde1fa9407838b2e589a2
Serejka_work <root@Bunker>
2017-03-10 05:43:39 +0300

    Added changes

--- scripts/task4/4_1.py
+++ scripts/task4/4_1.py
@@ -9,5 +9,10 @@ print "Network:"
 print "{:10} {:10} {:10} {:10} \n{:10} {:10} {:10} {:10}".format(net.split(".")[0], net.split(".")[1], net.split(".")[2], net.split(".")[3], bin(int(net.split(".")[0]))[2:].zfill(10), bin(int(net.split(".")[1]))[2:].zfill(10), bin(int(net.split(".")[2]))[2:].zfill(10), bin(int(net.split(".")[3]))[2:].zfill(10))
 
 
-stroka=115
-int(list(str(bin(stroka)))[3])
+
+# Вывести число
+#stroka=115
+#int(list(str(bin(stroka)))[3])
+
+
+# bin(int(net.split(".")[0]))[2:].zfill(10)
